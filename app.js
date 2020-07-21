26a2ad13e8ec7dea3333473faf8291cd222f26ed
fahadkhan917 <fk16181@gmail.com>
2020-07-21 15:01:23 +1000

    Update app.js

--- app.js
+++ app.js
@@ -8,18 +8,29 @@ var text=document.createTextNode(val.value);
 l.appendChild(text);
 list.append(l);
 
+
 val.value='';
 
 //deletebtn
 var delebtn=document.createElement('button');
 var deltext=document.createTextNode('delete button');
 delebtn.appendChild(deltext);
+delebtn.setAttribute("onclick","deletebtn(this)")
+
 
 l.appendChild(delebtn);
 
+//edit
+var editbtn=document.createElement('button');
+var edittext=document.createTextNode('Edit button');
+editbtn.appendChild(edittext);
+editbtn.setAttribute("onclick","ed(this)")
 
 
-list.append(l);
+l.appendChild(editbtn);
+
+
+
 
 
 
@@ -35,3 +46,16 @@ function del(){
 
 }
 
+function deletebtn(e){
+e.parentNode.remove();
+
+}
+
+
+function ed(e){
+    
+    //  var edi=prompt("enter the change");
+    console.log(list.childNodes[1].childNodes[0]);
+    console.log(e.parentNode.first);
+    
+    }
