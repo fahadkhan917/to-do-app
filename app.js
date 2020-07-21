45fe0bd93158ee5796c8b6bc12c72a53a0eb41aa
fahadkhan917 <fk16181@gmail.com>
2020-07-21 21:26:22 +1000

    update

--- app.js
+++ app.js
@@ -7,25 +7,28 @@ var l=document.createElement('li');
 var text=document.createTextNode(val.value);
 l.appendChild(text);
 list.append(l);
+list.setAttribute("class","li");
+list.setAttribute("class",'txt');
 
 
 val.value='';
 
 //deletebtn
 var delebtn=document.createElement('button');
-var deltext=document.createTextNode('delete button');
+var deltext=document.createTextNode('delete');
+
 delebtn.appendChild(deltext);
 delebtn.setAttribute("onclick","deletebtn(this)")
-
+delebtn.setAttribute("class","btn btn-danger");
 
 l.appendChild(delebtn);
 
 //edit
 var editbtn=document.createElement('button');
-var edittext=document.createTextNode('Edit button');
+var edittext=document.createTextNode('Edit');
 editbtn.appendChild(edittext);
 editbtn.setAttribute("onclick","ed(this)")
-
+editbtn.setAttribute("class","btn btn-success");
 
 l.appendChild(editbtn);
 
@@ -39,7 +42,7 @@ l.appendChild(editbtn);
 function del(){
 
 
-    list.remove();
+    list.innerHTML='';
     
 
 
