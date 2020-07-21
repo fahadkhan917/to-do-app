84e85c3de23d56981e5ce82aa3a411b1c9311bb5
fahadkhan917 <fk16181@gmail.com>
2020-07-21 12:17:16 +1000

    update

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-function todo(){
 
-var 
+
+function todo(){
+var list=document.getElementById("list");
+var val=document.getElementById("val");
+
+var l=document.createElement('li');
+var text=document.createTextNode(val.value);
+l.appendChild(text);
+list.append(l);
+
+val.value='';
 
 
 }
