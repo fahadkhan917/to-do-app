7e01f79e298d9833c00665ea4b549fe0a1977375
fahadkhan917 <fk16181@gmail.com>
2020-07-21 13:35:36 +1000

    update

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 
 var list=document.getElementById("list");
 function todo(){
+    
 var val=document.getElementById("val");
-
 var l=document.createElement('li');
 var text=document.createTextNode(val.value);
 l.appendChild(text);
 list.append(l);
 
 val.value='';
+
+//deletebtn
+var delebtn=document.createElement('button');
+var deltext=document.createTextNode('delete button');
+delebtn.appendChild(deltext);
+
+l.appendChild(delebtn);
+
+
+
+list.append(l);
+
 
 
 }
@@ -22,3 +34,4 @@ function del(){
 
 
 }
+
